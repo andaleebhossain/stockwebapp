3e7c3fdd1de300072eaf3fdaccf7aa43924e9f3f
andaleebhossain <120740783+andaleebhossain@users.noreply.github.com>
2025-09-22 00:34:47 -0400

    Update app.py
    updated app, fixed yfinance bug

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from datetime import date
-
 import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
+import matplotlib.pyplot as plt
 
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
@@ -17,14 +17,12 @@ selected_stock = st.selectbox('Select dataset for prediction', stocks)
 n_years = st.slider('Years of prediction:', 1, 4)
 period = n_years * 365
 
-
-@st.cache
+@st.cache_data
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
     data.reset_index(inplace=True)
     return data
 
-	
 data_load_state = st.text('Loading data...')
 data = load_data(selected_stock)
 data_load_state.text('Loading data... done!')
@@ -32,33 +30,35 @@ data_load_state.text('Loading data... done!')
 st.subheader('Raw data')
 st.write(data.tail())
 
-# Plot raw data
-def plot_raw_data():
-	fig = go.Figure()
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-	st.plotly_chart(fig)
-	
-plot_raw_data()
+def plot_raw_data(data):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
 
-# Predict forecast with Prophet.
-df_train = data[['Date','Close']]
-df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+plot_raw_data(data)
 
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
+df_train = data[['Date', 'Close']].rename(columns={"Date": "ds", "Close": "y"})
 
-# Show and plot forecast
+@st.cache_resource
+def train_prophet(df):
+    m = Prophet()
+    m.fit(df)
+    return m
+
+with st.spinner('Training forecast model...'):
+    m = train_prophet(df_train)
+    future = m.make_future_dataframe(periods=period)
+    forecast = m.predict(future)
+
 st.subheader('Forecast data')
 st.write(forecast.tail())
-    
+
 st.write(f'Forecast plot for {n_years} years')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
 
 st.write("Forecast components")
 fig2 = m.plot_components(forecast)
-st.write(fig2)
+st.pyplot(fig2)
